0b4b29a8ec7ace253a1cbe35d5613aed9a7744f1
Obi1Wajz <leonspiljak69@gmail.com>
2026-03-19 13:03:52 +0100

    Add project files.

---
+++ BindingOfIsaacCheatSheetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BindingOfIsaacCheatSheetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
